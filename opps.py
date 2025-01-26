5fbb90f42fc396d4e82cab89410526338691e8f3
Yash <yashkhatri88540@gmail.com>
2025-01-26 16:12:36 +0530

    project completed

--- opps.py
+++ opps.py
@@ -1,13 +1,16 @@
-class employee:
-    def __init__(self): #constructer
-        self.id=123
-        self.salary=50000
-        self.designation= "SDE"
-    def travel(self, destination):
-        print(f"empolyee is now travelling to {destination} ")
+# class employee:
+#     def __init__(self): #constructer
+#         self.id=123
+#         self.salary=50000
+#         self.designation= "SDE"
+#     def travel(self, destination):
+#         print(f"empolyee is now travelling to {destination} ")
 
 
-sam=employee()
-print(sam.id)
+# sam=employee()
+# print(sam.id)
 
-sam.travel("manali")
+# sam.travel("manali")
+
+from proj import chatbook
+user1 =chatbook()
