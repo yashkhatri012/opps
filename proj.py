455b4506a2459a42920bc3e6c034cecc98525d4d
Yash <yashkhatri88540@gmail.com>
2025-01-26 15:47:01 +0530

    Signup and signin feature added

--- proj.py
+++ proj.py
@@ -13,17 +13,36 @@ class chatbook:
                            4.Press 4 to msg a friend
                            5.Press any other key to exit""")
         if user_input== "1":
-            pass
+            self.signup()
         elif user_input== "2":
-            pass
+            self.signin()
         elif user_input== "3":
             pass
         elif user_input== "4":
-            pass
+            pass 
         else:
             exit()
 
+    def signup(self):
+        email= input("Enter your email here -->  ")
+        pwd= input("setup your password : ")
+        self.username= email
+        self.password= pwd
+        print("You have signed up Successfully")
+        print("\n")  
+        self.menu()
 
-
-
+    def signin(self):
+        if self.username=='' and self.password == '':
+            print("Please signup first by pressing 1 in the main menu")
+        else:
+            uname= input("enter your email here --> ")
+            pwd = input("Enter your password here : ")
+            if self.username ==uname and self.password ==pwd:
+                print("You have signed in  successfully")
+                self.loggedin= True
+            else: 
+                print("Please input correct credentials..")
+        print("\n")  
+        self.menu()
 obj= chatbook()
